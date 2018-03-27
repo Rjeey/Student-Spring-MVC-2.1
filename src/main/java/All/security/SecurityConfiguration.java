4984aaa550a642529133d2f8c39f3f832b26ac22
имя_пользователя_github <licanomega@gail.com>
2018-03-27 15:22:30 +0300

     

---
+++ src/main/java/All/security/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package All.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("MyRjeey").password("Dummy").roles("USER", "ADMIN");
+        auth.inMemoryAuthentication().withUser("JustUser").password("1234").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/login").permitAll()
+                .antMatchers("/").access("hasRole('USER')").and().formLogin();
+        http.authorizeRequests().antMatchers("/login").permitAll()
+                .antMatchers("/*").access("hasRole('ADMIN')").and().formLogin();
+    }
+}
+
